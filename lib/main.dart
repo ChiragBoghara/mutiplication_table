f3b4d8a8a10c38ac1bf3586fee44e2bf55a9c4ba
chirag <chiragbogharait@gmail.com>
2020-12-30 11:09:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/main_Screen.dart';
+import 'screens/table_Screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
